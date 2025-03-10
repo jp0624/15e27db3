3766664d4b61c895545b6561786cc53a976f5144
John Parker <jparker@showcare.com>
2025-03-10 19:10:24 -0400

    adjusted some scss

--- src/App.tsx
+++ src/App.tsx
@@ -5,7 +5,7 @@ import { useState } from 'react';
 const App = () => {
   const [filter, setFilter] = useState<"active" | "archived">("active");
   return (
-    <div className='container'>
+    <div className='container w-full h-full md:max-h-[800px] md:max-w-[800px]'>
       <Header />
       <div className="container-main">
         <Home filter={filter} setFilter={setFilter} />
