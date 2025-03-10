be952703f38e052fe91f2592e3a13f4454c15a7e
John Parker <jparker@showcare.com>
2025-03-10 19:22:18 -0400

    minor edits

--- src/App.tsx
+++ src/App.tsx
@@ -5,7 +5,7 @@ import { useState } from 'react';
 const App = () => {
   const [filter, setFilter] = useState<"active" | "archived">("active");
   return (
-    <div className='container w-full h-full md:max-h-[800px] md:max-w-[800px]'>
+    <div className='container w-full h-full '>
       <Header />
       <div className="container-main">
         <Home filter={filter} setFilter={setFilter} />
